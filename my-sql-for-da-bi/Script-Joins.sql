fcfcb2f7089d862e7b5fd3770ae8a024bbba8f54
oscar.alejandro <oscarale930719@gmail.com>
2023-08-31 13:40:19 -0600

    Update Script-Joins.sql

--- my-sql-for-da-bi/Script-Joins.sql
+++ my-sql-for-da-bi/Script-Joins.sql
@@ -10,7 +10,9 @@ first type of data (for example in our database, the key emp_no), this is:
 
 USE employees;
 
-DROP TABLE departments_dup;
+# Set Up ‘departments_dup’ table
+
+DROP TABLE IF EXISTS departments_dup;
 
 CREATE TABLE departments_dup (
 	dept_no CHAR(4),
@@ -26,9 +28,33 @@ DELETE FROM departments_dup
 WHERE dept_no = 'd002';
 
 INSERT INTO departments_dup (dept_no)
-VALUES ("d010");
+VALUES ("d010"),
+("d011")
+;
 
-INSERT INTO departments_dup (dept_no)
-VALUES ("d011");
+
+# Create and fill the "dept_manager_dup" table
+DROP TABLE IF EXISTS dept_manager_dup;
+
+CREATE TABLE dept_manager_dup (
+	emp_no INT(11) NOT NULL,
+	dept_no CHAR(4) NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NULL
+);
+
+INSERT INTO dept_manager_dup 
+SELECT * FROM dept_manager;
+
+INSERT INTO dept_manager_dup (emp_no, from_date)
+VALUES (999904, '2017-01-01'),
+(999905, '2017-01-01'),
+(999906, '2017-01-01'),
+(999907, '2017-01-01')
+;
+
+DELETE FROM dept_manager_dup
+WHERE dept_no = 'd001';
+
 
 
